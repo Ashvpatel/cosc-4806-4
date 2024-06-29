bb2d0a127b837a49a6d7e2d4b14862a82232269f
Ashvpatel <ashvpatel@algomau.ca>
2024-06-29 00:44:45 +0000

    created a table which displays all the data in reminders table

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -5,9 +5,8 @@ class Reminders extends Controller {
     public function index() {
       $reminder = $this->model('Reminder');
       $list_of_reminders = $reminder->get_all_reminders();
-      print_r($list_of_reminders);
-      die;
-      $this->view('reminders/index');
+
+      $this->view('reminders/index', ['reminders' => $list_of_reminders]);
     }
 }
 ?>
--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -1,13 +1,38 @@
-<?php require_once 'app/views/templates/header.php' ?>
+<?php require_once 'app/views/templates/header.php'; ?>
 <div class="container">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
                 <h1>Reminders</h1>
-                <p> <a href="/reminders/create">Create a new reminder</a></p>
+                <p><a href="/reminders/create">Create a new reminder</a></p>
             </div>
         </div>
     </div>
 
-    <?php require_once 'app/views/templates/footer.php' ?>
-                       
+    <?php if (!empty($data['reminders'])): ?>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">user id</th>
+                    <th scope="col">subject</th>
+                    <th scope="col">created at</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($data['reminders'] as $reminder): ?>
+                    <tr>
+                        <th scope="row"><?php echo htmlspecialchars($reminder['id']); ?></th>
+                        <td><?php echo htmlspecialchars($reminder['user_id']); ?></td>
+                        <td><?php echo htmlspecialchars($reminder['subject']); ?></td>
+                        <td><?php echo htmlspecialchars($reminder['created_at']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No reminders found.</p>
+    <?php endif; ?>
+
+</div>
+<?php require_once 'app/views/templates/footer.php'; ?>
